128cad3cc9c1355cec23b4ba4d65c900ed30bd84
VladP <oqserboom@gmail.com>
2024-04-23 21:39:41 +0300

    finished routers test zone, w/o CSS but will be rdy soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,51 @@
-import React, {useState} from 'react'
 import './App.css'
-import { Routes, Route, Link } from 'react-router-dom';
-import { HomePage, UsersPage } from './Pages'
+import { Outlet, Link } from "react-router-dom";
 
-function App() {
-
-  return (
-      <>
-          <div>
-
-          <h1>h1asdasd</h1>
-          <header>
-              <Link to="/">Home</Link>
-              <Link to="/Users">Users</Link>
-              <Link to="/">About</Link>
-          </header>
-          <Routes>
-              <Route path="/" element={<HomePage/>}/>
-              <Route path="/Users" element={<UsersPage/>}/>
-          </Routes>
-          </div>
-      </>
-  )
+export default function App() {
+    return (
+        <>
+            <div id="sidebar">
+                <h1>iQ-platform test zone</h1>
+                <div>
+                    <form id="search-form" role="search">
+                        <input
+                            id="q"
+                            aria-label="Search contacts"
+                            placeholder="Не ищи, не работает"
+                            type="search"
+                            name="q"
+                        />
+                        <div
+                            id="search-spinner"
+                            aria-hidden
+                            hidden={true}
+                        />
+                        <div
+                            className="sr-only"
+                            aria-live="polite"
+                        ></div>
+                    </form>
+                    <form method="post" >
+                        <button type="submit">Новый пользователь</button>
+                    </form>
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to={`home`}>Home page</Link>
+                        </li>
+                        <li>
+                            <Link to={`users`}>Users table</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div id="detail">
+                <Outlet />
+            </div>
+        </>
+    );
 }
-
-export default App
 
 
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage  from "./Pages/HomePage/HomePage.tsx";
 import UsersPage from "./Pages/UsersPage/UsersPage.tsx";
+import ErrorPage from "./Pages/ErrorPage/ErrorPage.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
 
-// ReactDOM.createRoot(document.getElementById('users-list')!).render(<AppUsers />)
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage/>,
+        children: [
+            { path:"home", element: <HomePage /> },
+            { path:"users", element: <UsersPage /> },
+        ]
+    },
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+        <RouterProvider router={router} />
+);
