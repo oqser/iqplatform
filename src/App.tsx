61627deb58d4178ba8b75ae290e4e89cf74f2576
VladP <oqserboom@gmail.com>
2024-04-24 18:58:11 +0300

    Add app Bar test

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
+import React from 'react';
 import './App.css'
 import { Outlet, Link } from "react-router-dom";
+import { ButtonAppBar } from "./Components";
 
 export default function App() {
     return (
         <>
+            < ButtonAppBar />
             <div id="sidebar">
-                <h1>iQ-platform test zone</h1>
-                <div>
-                    <form id="search-form" role="search">
-                        <input
-                            id="q"
-                            aria-label="Search contacts"
-                            placeholder="Не ищи, не работает"
-                            type="search"
-                            name="q"
-                        />
-                        <div
-                            id="search-spinner"
-                            aria-hidden
-                            hidden={true}
-                        />
-                        <div
-                            className="sr-only"
-                            aria-live="polite"
-                        ></div>
-                    </form>
-                    <form method="post" >
-                        <button type="submit">Новый пользователь</button>
-                    </form>
-                </div>
                 <nav>
                     <ul>
                         <li>
